912428a67ae9f12838c316a291c3c559d88b8d0d
shenzhang <shenzhang722@gamil.com>
2015-03-23 10:25:25 +0800

    init project

---
+++ src/main/java/com/github/shenzhang/ejdbc/config/feature/GeneratedKeyFetcher.java
@@ -0,0 +1,12 @@
+package com.github.shenzhang.ejdbc.config.feature;
+
+import temple.sql.JdbcTemplateEnhancement;
+
+/**
+ * User: shenzhang
+ * Date: 9/6/14
+ * Time: 6:13 PM
+ */
+public interface GeneratedKeyFetcher {
+    long getGeneratedKey(JdbcTemplateEnhancement jdbcTemplateEnhancement, String table, String column);
+}
