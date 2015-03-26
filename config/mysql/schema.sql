fe9019bd0e71c4335456fc22a706d44c9479c893
shenzhang <shenzhang722@gamil.com>
2015-03-26 16:02:12 +0800

    Support mysql database

--- config/mysql/schema.sql
+++ config/mysql/schema.sql
@@ -1,7 +1,11 @@
+drop table if exists t_user;
+
 create table t_user
 (
-  id bigserial primary key,
+  id int not null auto_increment,
   name varchar(255) not null,
   age integer not null,
-  null_value integer
+  null_value integer,
+
+  primary key(id)
 );
